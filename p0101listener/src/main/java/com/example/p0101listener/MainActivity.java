369ac8e7981eab886517d0f6afcb89b965822924
dmitry.molchanov <penopllast@mail.ru>
2016-04-14 15:14:37 +0300

    10 и 11 упражнения пройдены

---
+++ p0101listener/src/main/java/com/example/p0101listener/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.p0101listener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // создание обработчика
+        View.OnClickListener oclBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.btnOk:
+                        tvOut.setText("Ок, я люблю ебать гусей");
+                        break;
+                    case R.id.btnCancel:
+                        tvOut.setText("Нет, мне не нравится ебать гусей");
+                        break;
+                }
+
+            }
+        };
+        btnOk.setOnClickListener(oclBtn);
+        btnCancel.setOnClickListener(oclBtn);
+
+    }
+}
