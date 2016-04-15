7c32d541b0168f0ccff848f530a7ddfd57496000
dmitry.molchanov <penopllast@mail.ru>
2016-04-15 09:49:05 +0300

    14.Динамическое меню

--- app/src/main/java/com/example/dm/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/dm/myfirstapp/MyActivity.java
@@ -3,6 +3,8 @@ package com.example.dm.myfirstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.dmitrymolchanov.myapplication.R;
+
 public class MyActivity extends AppCompatActivity {
 
     @Override
